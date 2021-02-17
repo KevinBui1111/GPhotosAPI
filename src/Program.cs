10b0e474a9b76408a94adce1bfd726c348c4d34d
Khanh Bui <Khanh.BuiD@homecredit.vn>
2021-02-17 17:49:11 +0700

    first commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GPhotosAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
